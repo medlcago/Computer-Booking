233caa57f98c06bbc20eda20521e684f50ab696e
medlcago <sasha.vol40@mail.ru>
2023-10-25 22:10:32 +0300

    Added password validator

--- api/src/routers/users/schemas.py
+++ api/src/routers/users/schemas.py
@@ -1,34 +1,52 @@
 from datetime import datetime
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 
 class BaseUser(BaseModel):
     user_id: int
     first_name: str
     last_name: str
-    created_at: datetime = None
+    created_at: datetime = Field(default_factory=datetime.utcnow)
 
     class Config:
         from_attributes = True
 
 
 class CreateUser(BaseUser):
-    password: str = Field(min_length=8, max_length=30, description="Пароль пользователя")
+    password: str = Field(
+        min_length=8,
+        max_length=30,
+        description="Пароль пользователя")
+
+    @field_validator("password")
+    def password_complexity(cls, password):
+        if not any(char.isupper() for char in password):
+            raise ValueError("Пароль должен содержать хотя бы одну заглавную букву")
+        if not any(char.isdigit() for char in password):
+            raise ValueError("Пароль должен содержать хотя бы одну цифру")
+        return password
 
 
 class ChangePasswordData(BaseModel):
     password: str = Field(min_length=8, max_length=30, description="Текущий пароль")
     new_password: str = Field(min_length=8, max_length=30, description="Новый пароль")
 
+    @field_validator("new_password")
+    def password_complexity(cls, new_password):
+        if not any(char.isupper() for char in new_password):
+            raise ValueError("Пароль должен содержать хотя бы одну заглавную букву")
+        if not any(char.isdigit() for char in new_password):
+            raise ValueError("Пароль должен содержать хотя бы одну цифру")
+        return new_password
+
     class Config:
         from_attributes = True
 
 
 class ChangePasswordResponse(BaseModel):
-    status: bool = Field(default=True, description="Статус операции")
     user_id: int = Field(description="ID пользователя")
-    new_password: str = Field(min_length=8, max_length=30, description="Новый пароль")
+    new_password: str = Field(description="Новый пароль")
 
     class Config:
         from_attributes = True
--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from environs import Env
 env = Env()
 env.read_env(".env")
 
-DB_HOST = env.str("host")
-DB_PORT = env.str("port")
-DB_USER = env.str("user")
-DB_PASSWORD = env.str("password")
-DB_NAME = env.str("db")
+DB_HOST = env.str("DB_HOST")
+DB_PORT = env.str("DB_PORT")
+DB_USER = env.str("DB_USER")
+DB_PASSWORD = env.str("DB_PASSWORD")
+DB_NAME = env.str("DB_NAME")
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
