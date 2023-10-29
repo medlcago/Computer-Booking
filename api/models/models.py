c428e4936941f6d122593308db1f134e77dec3b5
medlcago <sasha.vol40@mail.ru>
2023-10-29 16:06:03 +0300

    Minor changes to table schemas

--- api/models/models.py
+++ api/models/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy import Column, BIGINT, ForeignKey, TIMESTAMP, MetaData, String, INTEGER, BOOLEAN
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.orm import relationship, declarative_base, Mapped
 
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
@@ -15,10 +15,11 @@ class User(Base):
     first_name: str = Column(String(length=255), nullable=False)
     last_name: str = Column(String(length=255), nullable=False)
     password: str = Column(String(length=255), nullable=False)
-    created_at: datetime = Column(TIMESTAMP, nullable=False, default=datetime.utcnow)
-    updated_at: datetime = Column(TIMESTAMP, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at: datetime = Column(TIMESTAMP(timezone=True), nullable=False, default=datetime.utcnow)
+    updated_at: datetime = Column(TIMESTAMP(timezone=True), nullable=False, default=datetime.utcnow,
+                                  onupdate=datetime.utcnow)
 
-    booking = relationship("Booking", back_populates="user")
+    booking: Mapped["Booking"] = relationship("Booking", back_populates="user")
 
 
 class Computer(Base):
@@ -34,9 +35,9 @@ class Computer(Base):
     gpu: str = Column(String(length=255), nullable=False)
     description: str = Column(String(length=255), default=None)
     category: str = Column(String(length=255), nullable=False)
-    status: bool = Column(BOOLEAN, default=True)
+    is_reserved: bool = Column(BOOLEAN, nullable=False, default=False)
 
-    booking = relationship("Booking", back_populates="computer")
+    booking: Mapped["Booking"] = relationship("Booking", back_populates="computer")
 
 
 class Booking(Base):
@@ -45,8 +46,8 @@ class Booking(Base):
     id: int = Column(BIGINT, primary_key=True, index=True)
     user_id: int = Column(BIGINT, ForeignKey(User.user_id))
     computer_id: int = Column(BIGINT, ForeignKey(Computer.computer_id))
-    start_time: datetime = Column(TIMESTAMP, nullable=False)
-    end_time: datetime = Column(TIMESTAMP, nullable=False)
+    start_time: datetime = Column(TIMESTAMP(timezone=True), nullable=False)
+    end_time: datetime = Column(TIMESTAMP(timezone=True), nullable=False)
 
-    user = relationship("User", back_populates="booking")
-    computer = relationship("Computer", back_populates="booking")
+    user: Mapped["User"] = relationship("User", back_populates="booking")
+    computer: Mapped["Computer"] = relationship("Computer", back_populates="booking")
